f632aa6a7978f2f75e2a1638b6ef73338fbc35dc
Rui Guedes <ruiguedes12@hotmail.com>
2019-03-05 20:11:15 +0000

    Updated file information

--- upload_image.sh
+++ upload_image.sh
@@ -3,8 +3,8 @@
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=tiagoboldt # Replace by your docker hub username
-IMAGE_NAME=lbaw-piu
+DOCKER_USERNAME=ruiguedes # Replace by your docker hub username
+IMAGE_NAME=lbaw1854
 
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
