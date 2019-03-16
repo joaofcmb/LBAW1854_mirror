fd0eaed155403dfb144506f8bce26c7728fb356c
César Pinho <cesar98alexandre@gmail.com>
2019-03-16 23:29:15 +0000

    fixed edit task page

--- html/JS/main.js
+++ html/JS/main.js
@@ -1,10 +1,10 @@
-$('.main-tab > .collapse').on('show.bs.collapse', function() {
-    $(this).parent().css('background-color', '#f1f2f3')
-})
+// $('.main-tab > .collapse').on('show.bs.collapse', function() {
+//     $(this).parent().css('background-color', '#f1f2f3')
+// })
 
-$('.main-tab > .collapse').on('hide.bs.collapse', function() {
-    $(this).parent().css('background-color', '#f8f9fa')
-})
+// $('.main-tab > .collapse').on('hide.bs.collapse', function() {
+//     $(this).parent().css('background-color', '#f8f9fa')
+// })
 
 $('#progress-form input').on('input', function() {
     $("#progress-form label").text($(this)[0].value);
