3e621c9308632a5dc897951f622fa446828d5e78
César Pinho <cesar98alexandre@gmail.com>
2019-03-23 20:27:49 +0000

    Update database file

--- sql/datebase.sql
+++ sql/datebase.sql
@@ -1,11 +1,11 @@
 -- DROPs
 
-DROP TABLE IF EXISTS "User", Administrator, Developer CASCADE;
+DROP TABLE IF EXISTS "user", Administrator, Developer CASCADE;
 DROP TABLE IF EXISTS RemovedDeveloper, ProjectManager,TeamMember CASCADE;
 DROP TABLE IF EXISTS Follow, Project, Favorite, Forum, Thread CASCADE;
 DROP TABLE IF EXISTS Comment, TaskComment, ThreadComment CASCADE;
 DROP TABLE IF EXISTS Task, Team, TeamProject, TeamTask CASCADE;
-DROP TABLE IF EXISTS TaskGroup, Milestone CASCADE;
+DROP TABLE IF EXISTS TaskGroup, Milestone, TeamLeader CASCADE;
 
 DROP TYPE IF EXISTS ProjectStatus CASCADE;
 
@@ -16,64 +16,65 @@ CREATE TYPE ProjectStatus AS ENUM ('active', 'canceled', 'closed');
 -- TABLES
 
 -- R01
-CREATE TABLE "User" (
+CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     username text NOT NULL CONSTRAINT username_uk UNIQUE,
     first_name text NOT NULL,
     last_anem text NOT NULL,
     email text NOT NULL,
-    password text NOT NULL
+    password text NOT NULL,
+    biography text
 );
 
 -- R02
 CREATE TABLE Administrator (
-    id_user INTEGER PRIMARY KEY REFERENCES "User" (id) ON UPDATE cascade
+    id_user INTEGER PRIMARY KEY REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R03
 CREATE TABLE Developer (
-    id_user INTEGER PRIMARY KEY REFERENCES "User" (id) ON UPDATE cascade
+    id_user INTEGER PRIMARY KEY REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R04
 CREATE TABLE RemovedDeveloper (
-    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE cascade
+    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R05
 CREATE TABLE ProjectManager (
-    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE cascade
+    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R08
 CREATE TABLE Follow (
-    id_follower INTEGER REFERENCES "User" (id) ON UPDATE cascade,
-    id_followee INTEGER REFERENCES "User" (id) ON UPDATE cascade,
+    id_follower INTEGER REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    id_followee INTEGER REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT follow_ck CHECK (id_followee <> id_follower),
-    CONSTRAINT follwo_pk PRIMARY KEY (id_follower, id_followee)
+    CONSTRAINT follow_pk PRIMARY KEY (id_follower, id_followee)
 );
 
 -- R09
 CREATE TABLE Project (
     id SERIAL PRIMARY KEY,
     name text NOT NULL CONSTRAINT project_name_uk UNIQUE,
-    color text NOT NULL DEFAULT '0,0,255',
+    color text NOT NULL DEFAULT '0000ff',
     description text NOT NULL, 
     status ProjectStatus NOT NULL DEFAULT 'active',
-    id_manager INTEGER REFERENCES ProjectManager ON UPDATE cascade
+    id_manager INTEGER REFERENCES ProjectManager ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 -- R10
 CREATE TABLE Favorite (
-    id_user INTEGER REFERENCES "User" ON UPDATE cascade,
-    id_project INTEGER REFERENCES Project ON UPDATE cascade,
+    id_user INTEGER REFERENCES "user" ON UPDATE CASCADE ON DELETE CASCADE,
+    id_project INTEGER REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT favorite_pk PRIMARY KEY (id_user, id_project)
 );
 
 -- R11
 CREATE TABLE Forum (
     id SERIAL PRIMARY KEY,
-    id_project INTEGER REFERENCES Project ON UPDATE cascade
+    id_project INTEGER REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R12
@@ -82,8 +83,8 @@ CREATE TABLE Thread (
     title text NOT NULL,
     description text NOT NULL,
     last_edit_date timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    id_author INTEGER REFERENCES "User" ON UPDATE cascade,
-    id_forum INTEGER REFERENCES Forum ON UPDATE cascade,
+    id_author INTEGER REFERENCES "user" ON UPDATE CASCADE ON DELETE SET NULL,
+    id_forum INTEGER REFERENCES Forum ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT thread_uk UNIQUE(title, id_forum)
 );
 
@@ -92,13 +93,13 @@ CREATE TABLE Comment (
     id SERIAL PRIMARY KEY,
     "text" text NOT NULL,
     last_edit_date timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    id_author INTEGER REFERENCES "User" ON UPDATE cascade
+    id_author INTEGER REFERENCES "user" ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 -- R14
 CREATE TABLE ThreadComment (
-    id_comment INTEGER REFERENCES Comment ON UPDATE cascade,
-    id_thread INTEGER REFERENCES Thread ON UPDATE cascade,
+    id_comment INTEGER REFERENCES Comment ON UPDATE CASCADE ON DELETE CASCADE,
+    id_thread INTEGER REFERENCES Thread ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT thread_comment_pk PRIMARY KEY (id_comment, id_thread)
 );
 
@@ -106,7 +107,7 @@ CREATE TABLE ThreadComment (
 CREATE TABLE TaskGroup (
     id SERIAL PRIMARY KEY,
     title text NOT NULL,
-    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE cascade,
+    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT task_group_uk UNIQUE(title, id_project)
 );
 
@@ -115,7 +116,7 @@ CREATE TABLE Milestone (
     id SERIAL PRIMARY KEY,
     name text NOT NULL,
     deadline date NOT NULL,
-    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE cascade,
+    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT milestone_uk UNIQUE(name, id_project)
 );
 
@@ -127,16 +128,16 @@ CREATE TABLE Task (
     progress INTEGER NOT NULL CONSTRAINT percentage_ck CHECK ((progress >= 0) AND (progress <= 100)),
     creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     last_edit_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE cascade,
-    id_group INTEGER NOT NULL REFERENCES TaskGroup ON UPDATE cascade,
-    id_milestone INTEGER NOT NULL REFERENCES Milestone ON UPDATE cascade,
+    id_project INTEGER NOT NULL REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE,
+    id_group INTEGER REFERENCES TaskGroup ON UPDATE CASCADE ON DELETE SET NULL ,
+    id_milestone INTEGER REFERENCES Milestone ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT task_uk UNIQUE(title, id_project)
 );
 
 -- R15
 CREATE TABLE TaskComment (
-    id_comment INTEGER REFERENCES Comment ON UPDATE cascade,
-    id_task INTEGER REFERENCES Task ON UPDATE cascade,
+    id_comment INTEGER REFERENCES Comment ON UPDATE CASCADE ON DELETE CASCADE,
+    id_task INTEGER REFERENCES Task ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT task_comment_pk PRIMARY KEY (id_comment, id_task)
 );
 
@@ -149,26 +150,26 @@ CREATE TABLE Team (
 
 -- R20
 CREATE TABLE TeamTask (
-    id_team INTEGER REFERENCES Team ON UPDATE cascade,
-    id_task INTEGER REFERENCES Task ON UPDATE cascade,
+    id_team INTEGER REFERENCES Team ON UPDATE CASCADE ON DELETE CASCADE,
+    id_task INTEGER REFERENCES Task ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT team_task_pk PRIMARY KEY (id_team, id_task)
 );
 
 -- R21
 CREATE TABLE TeamProject (
-    id_team INTEGER REFERENCES Team ON UPDATE cascade,
-    id_project INTEGER REFERENCES Project ON UPDATE cascade,
+    id_team INTEGER REFERENCES Team ON UPDATE CASCADE ON DELETE CASCADE,
+    id_project INTEGER REFERENCES Project ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT team_project_pk PRIMARY KEY (id_team, id_project)
 );
 
 -- R06
 CREATE TABLE TeamMember (
-    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE cascade,
-    id_team INTEGER REFERENCES Team (id) ON UPDATE cascade
+    id_developer INTEGER PRIMARY KEY REFERENCES Developer (id_user) ON UPDATE CASCADE ON DELETE CASCADE,
+    id_team INTEGER REFERENCES Team (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R07
 CREATE TABLE TeamLeader (
-    id_team_member INTEGER PRIMARY KEY REFERENCES TeamMember (id_developer) ON UPDATE cascade,
-    id_team INTEGER REFERENCES Team (id) ON UPDATE cascade
+    id_team_member INTEGER PRIMARY KEY REFERENCES TeamMember (id_developer) ON UPDATE CASCADE ON DELETE CASCADE,
+    id_team INTEGER REFERENCES Team (id) ON UPDATE CASCADE on DELETE CASCADE
 );
